03ba8edb37fc88e11e72539491ed7bfd1ddb366d
asd-35 <saridoganarda35@gmail.com>
2022-09-30 04:51:58 +0300

    for some fucking reason window in the game is not adjustable so i got procID's by using findWindowA and Tlhelp32.h snapshotting the processes and made sure that they match to sum up the name of the window for the game is Minesweeper and i can just type that to get the procID. ffs.

--- Mine.cpp
+++ Mine.cpp
@@ -1,7 +1,61 @@
 #include <iostream>
+#include <Windows.h>
+#include < TlHelp32.h>
+
+DWORD flagBaseAddr = 0x01005194;
+DWORD timeBaseAddr = 0x0100579C;
+
+DWORD procID;
+
+DWORD findProcId(char* procName) {
+    DWORD result = NULL;
+
+    PROCESSENTRY32 procEntry32;
+   
+    procEntry32.dwSize == sizeof(PROCESSENTRY32);
+
+    auto procSnap = CreateToolhelp32Snapshot(TH32CS_SNAPPROCESS, 0);
+
+    if (procSnap == INVALID_HANDLE_VALUE)
+    {
+        std::cout << "taking a snaproc failed" << std::endl;
+        return result;
+    }
+    else {
+        while (Process32Next(procSnap,&procEntry32))
+        {
+            if (!strcmp(procName, procEntry32.szExeFile)) {
+                result = procEntry32.th32ProcessID;
+                break;
+            }
+        }
+        return result;
+    }
+    
+    
+}
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    HWND hwnd = FindWindowA(NULL, "Minesweeper");
+
+    if (hwnd == NULL) {
+        std::cout << "lol";
+    }
+    else {
+        GetWindowThreadProcessId(hwnd, &procID);
+        if (findProcId((char*)"Winmine__XP.exe") == procID)
+        {
+            std::cout << "proc ID's match. One is from snap and one is from window name" << std::endl;
+            std::cout << "Getting a handle and changing the address values.." << std::endl;
+           
+
+
+        }
+    }
+
+
+
+
 }
 
